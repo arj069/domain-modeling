34c2eb108bdf4e297dcebe6948c85296edb5acd4
Abishek Raghavan <abishek.raghavan@kellogg.northwestern.edu>
2023-04-14 21:29:35 +0000

    Tacostagram update

--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,35 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
 
+CREATE TABLE users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_name TEXT,
+    real_name TEXT,
+    location TEXT
+);
+
+CREATE TABLE posts(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    image_url TEXT,
+    posted_at TEXT,
+    user_id INTEGER
+);
+
+CREATE TABLE likes(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    post_id INTEGER
+);
+
+CREATE TABLE comments(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    post_id INTEGER,
+    body TEXT
+);
+
+CREATE TABLE follows(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    follower_id INTEGER,
+    followed_id INTEGER
+);
